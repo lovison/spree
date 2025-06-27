9f2d10f85201d62613e13e13188d4b6cf59787c1
Gabriel Z. Lovison <gabriel.lovison@gmail.com>
2025-06-27 11:04:05 -0300

    Enable translations for Spree::Property
    - Add `Spree::Property` to the list of translatable resources
    - Implement translation form for property records
    - Fix typo in property code tag

--- admin/app/controllers/spree/admin/translations_controller.rb
+++ admin/app/controllers/spree/admin/translations_controller.rb
@@ -71,6 +71,8 @@ module Spree
             spree.edit_admin_option_type_path(@resource)
           when 'Spree::Product'
             spree.edit_admin_product_path(@resource)
+          when 'Spree::Property'
+            spree.edit_admin_option_type_path(@resource)
           when 'Spree::Taxon'
             spree.edit_admin_taxonomy_taxon_path(@resource.taxonomy, @resource.id)
           when 'Spree::Taxonomy'
